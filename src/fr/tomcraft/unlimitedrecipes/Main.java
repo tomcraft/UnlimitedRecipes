fd6467b9221f801b5717fb53a68470d6680a8c4f
Tomcraft <tomi91630@gmail.com>
2013-03-06 01:22:46 +0100

    Add permission

--- src/fr/tomcraft/unlimitedrecipes/Main.java
+++ src/fr/tomcraft/unlimitedrecipes/Main.java
@@ -11,44 +11,47 @@ import org.bukkit.inventory.Recipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
-	
+
 	public Config config;
 	public ArrayList<ItemStack> overidenCrafts;
 	public ArrayList<ItemStack> overidenSmelts;
-	
+
 	public void onEnable(){
 		overidenCrafts = new ArrayList<ItemStack>();
 		overidenSmelts = new ArrayList<ItemStack>();
 		config = new Config(this);
 		config.loadConfigs();
 	}
-	
+
 	public FileConfiguration getCraftingConfig()
 	{
 		return config.crafting;
 	}
-	
+
 	public FileConfiguration getFurnaceConfig()
 	{
 		return config.furnace;
 	}
-	
+
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
 	{
-		if(label.equalsIgnoreCase("ur"))
-		{
-			if(args.length == 0 || !args[0].equalsIgnoreCase("reload"))
+		if(sender.isOp() || sender.hasPermission("ur.reload")){
+
+			if(label.equalsIgnoreCase("ur"))
 			{
-				sender.sendMessage(ChatColor.RED+"Usage: /ur reload");
-				return false;
+				if(args.length == 0 || !args[0].equalsIgnoreCase("reload"))
+				{
+					sender.sendMessage(ChatColor.RED+"Usage: /ur reload");
+					return false;
+				}
+
+				this.getServer().resetRecipes();
+
+				this.config = null;
+
+				this.onEnable();
+
 			}
-			
-			this.getServer().resetRecipes();
-			
-			this.config = null;
-			
-			this.onEnable();
-			
 		}
 		return true;
 	}
